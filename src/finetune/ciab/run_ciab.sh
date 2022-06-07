3dc9e310d8990640d0e3953a2f55dec290ede256
harrygcoppock <harrygcoppock@gmail.com>
2022-06-07 09:49:06 +0000

    updated run_ciab for matched long

--- src/finetune/ciab/run_ciab.sh
+++ src/finetune/ciab/run_ciab.sh
@@ -1,7 +1,7 @@
 set -x
 # comment this line if not running on sls cluster
 #. /data/sls/scratch/share-201907/slstoolchainrc
-source ../../../venvssast/bin/activate
+#source ../../../venvssast/bin/activate
 export TORCH_HOME=../../pretrained_models
 mkdir exp
 
@@ -22,121 +22,135 @@ fi
 
 pretrain_exp=unknown
 pretrain_model=SSAST-Base-Frame-400
+modalities = ("ciab_sentence", "ciab_three_cough", "ciab_cough", "ciab_ha_sound")
+for dataset in "${modalities[*]}; do
+	if [ "$dataset" == ciab_three_cough ]
+	then
+		dataset_mean=-8.2096
+		dataset_std=6.1568
+		dir_name = audio_three_cough_url
+	elif [ "$dataset" == ciab_sentence ]
+	then	
+		dataset_mean=-7.8560
+		dataset_std=5.4546
+		dir_name = audio_sentence_url
+	elif [ "$dataset" == ciab_ha_sound ]
+	then
+		dataset_mean=-8.8335
+		dataset_std=5.9809
+		dir_name = audio_ha_sound_url
+	else
+		dataset_mean=-7.7893
+		dataset_std=6.3424
+		dir_name = audio_cough_url
+	fi
+	target_length=512
+	noise=True
 
-dataset=ciab_sentence
-if [ "$dataset" == ciab_three_cough ]
-then
-	dataset_mean=-8.2096
-	dataset_std=6.1568
-elif [ "$dataset" == ciab_sentence ]
-then	
-	dataset_mean=-7.8560
-	dataset_std=5.4546
-elif [ "$dataset" == ciab_ha_sound ]
-then
-	dataset_mean=-8.8335
-	dataset_std=5.9809
-else
-	dataset_mean=-7.7893
-	dataset_std=6.3424
-fi
-target_length=512
-noise=True
+	bal=none
+	lr=1e-4
+	freqm=24
+	timem=96
+	mixup=0
+	epoch=20
+	batch_size=20
+	fshape=128
+	tshape=2
+	fstride=128
+	tstride=1
 
-bal=none
-lr=1e-4
-freqm=24
-timem=96
-mixup=0
-epoch=20
-batch_size=20
-fshape=128
-tshape=2
-fstride=128
-tstride=1
+	task=ft_avgtok
+	model_size=base
+	head_lr=1
+	fold=1
+	pretrain_path=./${pretrain_model}.pth
+	base_exp_dir=./exp/final/test01-${dataset}-f${fstride}-${fshape}-t${tstride}-${tshape}-b${batch_size}-lr${lr}-${task}-${model_size}-${pretrain_exp}-${pretrain_model}-${head_lr}x-noise${noise}-standard-train-final2
 
-task=ft_avgtok
-model_size=base
-head_lr=1
-fold=1
-pretrain_path=./${pretrain_model}.pth
-base_exp_dir=./exp/final/test01-${dataset}-f${fstride}-${fshape}-t${tstride}-${tshape}-b${batch_size}-lr${lr}-${task}-${model_size}-${pretrain_exp}-${pretrain_model}-${head_lr}x-noise${noise}-standard-train-final2
+	echo 'now process fold'${fold}
 
-echo 'now process fold'${fold}
+	exp_dir=${base_exp_dir}/fold${fold}
 
-exp_dir=${base_exp_dir}/fold${fold}
+	train_data=./data/datafiles/${dir_name}/ciab_train_data_${fold}.json
+	validation_data=./data/datafiles/${dir_name}/ciab_validation_data_${fold}.json
+	test_data=./data/datafiles/${dir_name}/ciab_test_data_${fold}.json
+	matched_test_data=./data/datafiles/${dir_name}/ciab_matched_test_data_${fold}.json
+	matched_train_data=./data/datafiles/${dir_name}/dic_matched_train_list${fold}.json
+	matched_validation_data=./data/datafiles/${dir_name}/dic_matched_validation_list${fold}.json
+	long_test_data=./data/datafiles/${dir_name}/ciab_long_test_data_${fold}.json
+	matched_long_test_data=./data/datafiles/${dir_name}/ciab_long_matched_data_${fold}.json
+	naive_train_data=./data/datafiles/${dir_name}/naive_train_${fold}.json
+	naive_validation_data=./data/datafiles/${dir_name}/naive_validation_${fold}.json
+	naive_test_data=./data/datafiles/${dir_name}/naive_test_${fold}.json
 
-train_data=./data/datafiles/audio_sentence_url/ciab_train_data_${fold}.json
-validation_data=./data/datafiles/audio_sentence_url/ciab_validation_data_${fold}.json
-standard_test_data=./data/datafiles/audio_sentence_url/ciab_test_data_${fold}.json
-matched_test_data=./data/datafiles/audio_sentence_url/ciab_matched_test_data_${fold}.json
-matched_train_data=./data/datafiles/audio_sentence_url/ciab_matched_train_list${fold}.json
-matched_validation_data=./data/datafiles/audio_sentence_url/ciab_matched_validation_list${fold}.json
-long_test_data=./data/datafiles/audio_sentence_url/ciab_long_test_data_${fold}.json
-naive_train_data=./data/datafiles/audio_sentence_url/naive_train_${fold}.json
-naive_validation_data=./data/datafiles/audio_sentence_url/naive_validation_${fold}.json
-naive_test_data=./data/datafiles/audio_sentence_url/naive_test_${fold}.json
+	original_train_data=./data/datafiles/${dir_name}/ciab_train_original_data_${fold}.json
+	original_validation_data=./data/datafiles/${dir_name}/ciab_validation_original_data_${fold}.json
+	original_test_data=./data/datafiles/${dir_name}/ciab_test_original_data_${fold}.json
+	original_matched_train_data=./data/datafiles/${dir_name}/dic_matched_train_original_list${fold}.json
+	original_matched_validation_data=./data/datafiles/${dir_name}/dic_matched_validation_original_list${fold}.json
+	original_matched_test_data=./data/datafiles/${dir_name}/ciab_matched_test_original_data_${fold}.json
 
-# standard train
-CUDA_CACHE_DISABLE=1 python -W ignore ../../run.py --dataset ${dataset} \
---data-train ${train_data} --data-val ${validation_data} --data-standard-test ${standard_test_data} \
---data-matched-test ${matched_test_data} --exp-dir $exp_dir --data-long-test ${long_test_data} \
---label-csv ./data/ciab_class_labels_indices.csv --n_class 2 \
---lr $lr --n-epochs ${epoch} --batch-size $batch_size --save_model False \
---freqm $freqm --timem $timem --mixup ${mixup} --bal ${bal} \
---tstride $tstride --fstride $fstride --fshape ${fshape} --tshape ${tshape} --warmup False --task ${task} \
---model_size ${model_size} --adaptschedule False \
---pretrain False --pretrained_mdl_path ${pretrain_path} \
---dataset_mean ${dataset_mean} --dataset_std ${dataset_std} --target_length ${target_length} \
---num_mel_bins 128 --head_lr ${head_lr} --noise ${noise} \
---lrscheduler_start 6 --lrscheduler_step 1 --lrscheduler_decay 0.85 --wa False --loss CE --metrics mAP
+	# standard train
+	CUDA_CACHE_DISABLE=1 python -W ignore ../../run.py --dataset ${dataset} \
+	--data-train ${train_data} --data-val ${validation_data} --data-test ${test_data} \
+	--data-matched-test ${matched_test_data} --data-long-test ${long_test_data} \
+	--data-long-matched ${matched_long_test_data} \
+	--label-csv ./data/ciab_class_labels_indices.csv --n_class 2 --exp-dir $exp_dir \
+	--lr $lr --n-epochs ${epoch} --batch-size $batch_size --save_model False \
+	--freqm $freqm --timem $timem --mixup ${mixup} --bal ${bal} \
+	--tstride $tstride --fstride $fstride --fshape ${fshape} --tshape ${tshape} --warmup False --task ${task} \
+	--model_size ${model_size} --adaptschedule False \
+	--pretrain False --pretrained_mdl_path ${pretrain_path} \
+	--dataset_mean ${dataset_mean} --dataset_std ${dataset_std} --target_length ${target_length} \
+	--num_mel_bins 128 --head_lr ${head_lr} --noise ${noise} \
+	--lrscheduler_start 6 --lrscheduler_step 1 --lrscheduler_decay 0.85 --wa False --loss CE --metrics mAP
 
-##  matched train
-base_exp_dir=./exp/test01-${dataset}-f${fstride}-${fshape}-t${tstride}-${tshape}-b${batch_size}-lr${lr}-${task}-${model_size}-${pretrain_exp}-${pretrain_model}-${head_lr}x-noise${noise}-matched-train-final
-exp_dir=${base_exp_dir}/fold${fold}
-CUDA_CACHE_DISABLE=1 python -W ignore ../../run.py --dataset ${dataset} \
---data-train ${matched_train_data} --data-val ${matched_validation_data} \
---data-standard-test ${standard_test_data} --data-matched-test ${matched_test_data} --exp-dir $exp_dir \
---label-csv ./data/ciab_class_labels_indices.csv --n_class 2 --data-long-test ${long_test_data} \
---lr $lr --n-epochs ${epoch} --batch-size $batch_size --save_model False \
---freqm $freqm --timem $timem --mixup ${mixup} --bal ${bal} \
---tstride $tstride --fstride $fstride --fshape ${fshape} --tshape ${tshape} --warmup False --task ${task} \
---model_size ${model_size} --adaptschedule False \
---pretrain False --pretrained_mdl_path ${pretrain_path} \
---dataset_mean ${dataset_mean} --dataset_std ${dataset_std} --target_length ${target_length} \
---num_mel_bins 128 --head_lr ${head_lr} --noise ${noise} \
---lrscheduler_start 6 --lrscheduler_step 1 --lrscheduler_decay 0.85 --wa False --loss CE --metrics mAP
+	##  matched train
+	base_exp_dir=./exp/test01-${dataset}-f${fstride}-${fshape}-t${tstride}-${tshape}-b${batch_size}-lr${lr}-${task}-${model_size}-${pretrain_exp}-${pretrain_model}-${head_lr}x-noise${noise}-matched-train-final
+	exp_dir=${base_exp_dir}/fold${fold}
+	CUDA_CACHE_DISABLE=1 python -W ignore ../../run.py --dataset ${dataset} \
+	--data-train ${matched_train_data} --data-val ${matched_validation_data} \
+	--data-standard-test ${standard_test_data} --data-matched-test ${matched_test_data} --exp-dir $exp_dir \
+	--label-csv ./data/ciab_class_labels_indices.csv --n_class 2 --data-long-test ${long_test_data} \
+	--lr $lr --n-epochs ${epoch} --batch-size $batch_size --save_model False \
+	--freqm $freqm --timem $timem --mixup ${mixup} --bal ${bal} \
+	--tstride $tstride --fstride $fstride --fshape ${fshape} --tshape ${tshape} --warmup False --task ${task} \
+	--model_size ${model_size} --adaptschedule False \
+	--pretrain False --pretrained_mdl_path ${pretrain_path} \
+	--dataset_mean ${dataset_mean} --dataset_std ${dataset_std} --target_length ${target_length} \
+	--num_mel_bins 128 --head_lr ${head_lr} --noise ${noise} \
+	--lrscheduler_start 6 --lrscheduler_step 1 --lrscheduler_decay 0.85 --wa False --loss CE --metrics mAP
 
-# naive method
-base_exp_dir=./exp/test01-${dataset}-f${fstride}-${fshape}-t${tstride}-${tshape}-b${batch_size}-lr${lr}-${task}-${model_size}-${pretrain_exp}-${pretrain_model}-${head_lr}x-noise${noise}-naive-final
-exp_dir=${base_exp_dir}/fold${fold}
-CUDA_CACHE_DISABLE=1 python -W ignore ../../run.py --dataset ${dataset} \
---data-train ${naive_train_data} --data-val ${naive_validation_data} \
---data-standard-test ${naive_test_data} \
---exp-dir $exp_dir \
---label-csv ./data/ciab_class_labels_indices.csv --n_class 2 \
---lr $lr --n-epochs ${epoch} --batch-size $batch_size --save_model False \
---freqm $freqm --timem $timem --mixup ${mixup} --bal ${bal} \
---tstride $tstride --fstride $fstride --fshape ${fshape} --tshape ${tshape} --warmup False --task ${task} \
---model_size ${model_size} --adaptschedule False \
---pretrain False --pretrained_mdl_path ${pretrain_path} \
---dataset_mean ${dataset_mean} --dataset_std ${dataset_std} --target_length ${target_length} \
---num_mel_bins 128 --head_lr ${head_lr} --noise ${noise} \
---lrscheduler_start 6 --lrscheduler_step 1 --lrscheduler_decay 0.85 --wa False --loss CE --metrics mAP
+	# naive method
+	base_exp_dir=./exp/test01-${dataset}-f${fstride}-${fshape}-t${tstride}-${tshape}-b${batch_size}-lr${lr}-${task}-${model_size}-${pretrain_exp}-${pretrain_model}-${head_lr}x-noise${noise}-naive-final
+	exp_dir=${base_exp_dir}/fold${fold}
+	CUDA_CACHE_DISABLE=1 python -W ignore ../../run.py --dataset ${dataset} \
+	--data-train ${naive_train_data} --data-val ${naive_validation_data} \
+	--data-standard-test ${naive_test_data} \
+	--exp-dir $exp_dir \
+	--label-csv ./data/ciab_class_labels_indices.csv --n_class 2 \
+	--lr $lr --n-epochs ${epoch} --batch-size $batch_size --save_model False \
+	--freqm $freqm --timem $timem --mixup ${mixup} --bal ${bal} \
+	--tstride $tstride --fstride $fstride --fshape ${fshape} --tshape ${tshape} --warmup False --task ${task} \
+	--model_size ${model_size} --adaptschedule False \
+	--pretrain False --pretrained_mdl_path ${pretrain_path} \
+	--dataset_mean ${dataset_mean} --dataset_std ${dataset_std} --target_length ${target_length} \
+	--num_mel_bins 128 --head_lr ${head_lr} --noise ${noise} \
+	--lrscheduler_start 6 --lrscheduler_step 1 --lrscheduler_decay 0.85 --wa False --loss CE --metrics mAP
 
-# train = train + long  method
-base_exp_dir=./exp/test01-${dataset}-f${fstride}-${fshape}-t${tstride}-${tshape}-b${batch_size}-lr${lr}-${task}-${model_size}-${pretrain_exp}-${pretrain_model}-${head_lr}x-noise${noise}-big-final
-exp_dir=${base_exp_dir}/fold${fold}
-CUDA_CACHE_DISABLE=1 python -W ignore ../../run.py --dataset ${dataset} \
---data-train ${big_train_data} --data-val ${big_validation_data} \
---data-standard-test ${standard_test_data} --data-matched-test ${matched_test_data} \
---exp-dir $exp_dir \
---label-csv ./data/ciab_class_labels_indices.csv --n_class 2 \
---lr $lr --n-epochs ${epoch} --batch-size $batch_size --save_model False \
---freqm $freqm --timem $timem --mixup ${mixup} --bal ${bal} \
---tstride $tstride --fstride $fstride --fshape ${fshape} --tshape ${tshape} --warmup False --task ${task} \
---model_size ${model_size} --adaptschedule False \
---pretrain False --pretrained_mdl_path ${pretrain_path} \
---dataset_mean ${dataset_mean} --dataset_std ${dataset_std} --target_length ${target_length} \
---num_mel_bins 128 --head_lr ${head_lr} --noise ${noise} \
---lrscheduler_start 6 --lrscheduler_step 1 --lrscheduler_decay 0.85 --wa False --loss CE --metrics mAP
+	# train = train + long  method
+	base_exp_dir=./exp/test01-${dataset}-f${fstride}-${fshape}-t${tstride}-${tshape}-b${batch_size}-lr${lr}-${task}-${model_size}-${pretrain_exp}-${pretrain_model}-${head_lr}x-noise${noise}-big-final
+	exp_dir=${base_exp_dir}/fold${fold}
+	CUDA_CACHE_DISABLE=1 python -W ignore ../../run.py --dataset ${dataset} \
+	--data-train ${big_train_data} --data-val ${big_validation_data} \
+	--data-standard-test ${standard_test_data} --data-matched-test ${matched_test_data} \
+	--exp-dir $exp_dir \
+	--label-csv ./data/ciab_class_labels_indices.csv --n_class 2 \
+	--lr $lr --n-epochs ${epoch} --batch-size $batch_size --save_model False \
+	--freqm $freqm --timem $timem --mixup ${mixup} --bal ${bal} \
+	--tstride $tstride --fstride $fstride --fshape ${fshape} --tshape ${tshape} --warmup False --task ${task} \
+	--model_size ${model_size} --adaptschedule False \
+	--pretrain False --pretrained_mdl_path ${pretrain_path} \
+	--dataset_mean ${dataset_mean} --dataset_std ${dataset_std} --target_length ${target_length} \
+	--num_mel_bins 128 --head_lr ${head_lr} --noise ${noise} \
+	--lrscheduler_start 6 --lrscheduler_step 1 --lrscheduler_decay 0.85 --wa False --loss CE --metrics mAP
+done
