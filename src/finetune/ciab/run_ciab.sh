9116ef9d10293eec5a74b0a4cce2bf9275abc61a
harrygcoppock <harrygcoppock@gmail.com>
2022-06-16 14:57:41 +0000

    progress with sonify plot now just need to play sound back

--- src/finetune/ciab/run_ciab.sh
+++ src/finetune/ciab/run_ciab.sh
@@ -22,27 +22,27 @@ fi
 
 pretrain_exp=unknown
 pretrain_model=SSAST-Base-Frame-400
-modalities = ("ciab_sentence", "ciab_three_cough", "ciab_cough", "ciab_ha_sound")
-for dataset in "${modalities[*]}; do
+for dataset in ciab_sentence ciab_three_cough ciab_cough ciab_ha_sound; do
+	echo $dataset
 	if [ "$dataset" == ciab_three_cough ]
 	then
 		dataset_mean=-8.2274
 		dataset_std=6.1628
-		dir_name = audio_three_cough_url
+		dir_name=audio_three_cough_url
 	elif [ "$dataset" == ciab_sentence ]
 	then	
 		dataset_mean=-7.8505
 		dataset_std=5.4594
-		dir_name = audio_sentence_url
+		dir_name=audio_sentence_url
 	elif [ "$dataset" == ciab_ha_sound ]
 	then
 		dataset_mean=-8.8451
 		dataset_std=5.9847
-		dir_name = audio_ha_sound_url
+		dir_name=audio_ha_sound_url
 	else
 		dataset_mean=-7.7998
 		dataset_std=6.3570
-		dir_name = audio_cough_url
+		dir_name=audio_cough_url
 	fi
 	target_length=512
 	noise=True
@@ -69,7 +69,6 @@ for dataset in "${modalities[*]}; do
 	echo 'now process fold'${fold}
 
 	exp_dir=${base_exp_dir}/fold${fold}
-
 	train_data=./data/datafiles/${dir_name}/ciab_train_data_${fold}.json
 	validation_data=./data/datafiles/${dir_name}/ciab_validation_data_${fold}.json
 	test_data=./data/datafiles/${dir_name}/ciab_test_data_${fold}.json
@@ -109,7 +108,8 @@ for dataset in "${modalities[*]}; do
 	exp_dir=${base_exp_dir}/fold${fold}
 	CUDA_CACHE_DISABLE=1 python -W ignore ../../run.py --dataset ${dataset} \
 	--data-train ${matched_train_data} --data-val ${matched_validation_data} \
-	--data-standard-test ${standard_test_data} --data-matched-test ${matched_test_data} --exp-dir $exp_dir \
+	--data-test ${test_data} --data-matched-test ${matched_test_data} \
+	--data-long-matched ${matched_long_test_data} --exp-dir $exp_dir \
 	--label-csv ./data/ciab_class_labels_indices.csv --n_class 2 --data-long-test ${long_test_data} \
 	--lr $lr --n-epochs ${epoch} --batch-size $batch_size --save_model False \
 	--freqm $freqm --timem $timem --mixup ${mixup} --bal ${bal} \
@@ -125,7 +125,7 @@ for dataset in "${modalities[*]}; do
 	exp_dir=${base_exp_dir}/fold${fold}
 	CUDA_CACHE_DISABLE=1 python -W ignore ../../run.py --dataset ${dataset} \
 	--data-train ${naive_train_data} --data-val ${naive_validation_data} \
-	--data-standard-test ${naive_test_data} \
+	--data-test ${naive_test_data} \
 	--exp-dir $exp_dir \
 	--label-csv ./data/ciab_class_labels_indices.csv --n_class 2 \
 	--lr $lr --n-epochs ${epoch} --batch-size $batch_size --save_model False \
@@ -143,7 +143,7 @@ for dataset in "${modalities[*]}; do
 	exp_dir=${base_exp_dir}/fold${fold}
 	CUDA_CACHE_DISABLE=1 python -W ignore ../../run.py --dataset ${dataset} \
 	--data-train ${original_train_data} --data-val ${original_validation_data} \
-	--data-test ${original_test_data} --data-matched-test ${original_matched_test_data} \ 
+	--data-test ${original_test_data} --data-matched-test ${original_matched_test_data} \
 	--data-long-matched ${matched_long_test_data} --exp-dir $exp_dir \
 	--label-csv ./data/ciab_class_labels_indices.csv --n_class 2 --data-long-test ${long_test_data} \
 	--lr $lr --n-epochs ${epoch} --batch-size $batch_size --save_model False \
@@ -160,7 +160,7 @@ for dataset in "${modalities[*]}; do
 	exp_dir=${base_exp_dir}/fold${fold}
 	CUDA_CACHE_DISABLE=1 python -W ignore ../../run.py --dataset ${dataset} \
 	--data-train ${original_matched_train_data} --data-val ${original_matched_validation_data} \
-	--data-test ${original_test_data} --data-matched-test ${original_matched_test_data} \ 
+	--data-test ${original_test_data} --data-matched-test ${original_matched_test_data} \
 	--data-long-matched ${matched_long_test_data} --exp-dir $exp_dir \
 	--label-csv ./data/ciab_class_labels_indices.csv --n_class 2 --data-long-test ${long_test_data} \
 	--lr $lr --n-epochs ${epoch} --batch-size $batch_size --save_model False \
@@ -170,4 +170,6 @@ for dataset in "${modalities[*]}; do
 	--pretrain False --pretrained_mdl_path ${pretrain_path} \
 	--dataset_mean ${dataset_mean} --dataset_std ${dataset_std} --target_length ${target_length} \
 	--num_mel_bins 128 --head_lr ${head_lr} --noise ${noise} \
+	--lrscheduler_start 6 --lrscheduler_step 1 --lrscheduler_decay 0.85 --wa False --loss CE --metrics mAP
 done
+
